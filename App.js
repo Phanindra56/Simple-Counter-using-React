7b9ff07c13a7bdacf19aa60f11a2cf6fd74bb8e8
Phanindra Dokku <phanindradokku@gmail.com>
2024-03-09 22:41:47 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+// import { Component } from 'react';
+import './App.css';
+// import LearningComponent from './Components/Learning Examples/LearningComponent'
+import Counter from './Components/Counter/Counter'
+// import Shopping from './Components/shopping/Shopping';
+function App() {
+  return (
+    <div>
+    {/* <LearningComponent/> */}
+    {/* <PlayingWithProps property1="value1" property2="value2"/> */}
+    <Counter />
+    {/* <Shopping /> */}
+    </div>
+  );
+}
+
+// function PlayingWithProps({property1,property2}){
+//     console.log(property1)
+//     console.log(property2)
+// }
+
+export default App;
